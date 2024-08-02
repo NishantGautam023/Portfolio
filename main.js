8cc1a8e38d085d614930f0c8faa772153b509f2d
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-08-02 11:31:47 -0400

    version1.0

--- main.js
+++ main.js
@@ -1,51 +1,64 @@
-// Select the dynamic text element where the typing effect will display
+import posthog from 'posthog-js'
+
+
+
+
+posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
+    api_host: import.meta.env.VITE_POSTHOG_HOST,
+    person_profiles: 'identified_only'
+  });
+
+// Typing Effect
+
+
 const dynamicText = document.querySelector("#intro-h2 span");
 
-// Array of words for the typing effect
+
 const words = ["Programmer", "Learner", "Enthusiast", "Engineer"];
 
-// Variables to track the current word, character index, and deletion status
+
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
 
-// Function to execute the typing effect
+
 const typeEffect = () => {
-    // Get the current word based on the word index
+   
     const currentWord = words[wordIndex];
 
-    // Get the part of the word to be displayed based on the character index
+
     const currentChar = currentWord.substring(0, charIndex);
 
-    // Update the text content of the dynamic text element
+
     dynamicText.textContent = currentChar;
 
-    // Temporarily stop the blinking cursor effect during typing/deleting
+   
     dynamicText.classList.add("stop-blinking");
 
     if (!isDeleting && charIndex < currentWord.length) {
-        // If not currently deleting and the entire word has not been typed out, type the next character
+       
         charIndex++;
-        setTimeout(typeEffect, 200); // Continue the effect with a short delay
+        setTimeout(typeEffect, 200); 
     } else if (isDeleting && charIndex > 0) {
-        // If currently deleting and there are characters left, remove the last character
+      
         charIndex--;
-        setTimeout(typeEffect, 100); // Continue the effect with a shorter delay for deletion
+        setTimeout(typeEffect, 100);
     } else {
-        // If the word has been completely typed out or deleted, switch modes
+      
         isDeleting = !isDeleting;
         dynamicText.classList.remove("stop-blinking");
 
-        // Move to the next word if we just finished typing one out, not deleting
+    
         wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
 
-        // Pause before starting to type the next word or before starting to delete
+     
         setTimeout(typeEffect, 1200);
     }
 };
 
-// Initiate the typing effect when the script loads
+
 typeEffect();
+
 
 
 
